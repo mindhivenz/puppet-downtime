e2e8fbe764450d3e57ed769e4c1b8a7259d07f63
damon <damon@mahuika.co.nz>
2020-05-28 15:38:05 +1200

    New format for specifying downtime window

--- functions/in_window.pp
+++ functions/in_window.pp
@@ -2,51 +2,6 @@ function downtime::in_window () >> Boolean {
 
   include downtime
 
-  if !$downtime::restricted {
-    debug('Not restricted')
-    true
-  } elsif $facts['timezone'] == 'UTC' {
-    debug('Timezone is UTC, reporting as not in window until facts represent requested timezone')
-    false
-  } else {
-    $tz = $facts['timezone']
-    $window = $downtime::window
-
-    $now = Timestamp(time())
-
-    notice('Now', $now)
-
-    $today_date = $now.strftime('%F', $tz)
-    $hours = if 'hours' in $window {
-      $window['hours']
-    } else {
-      1.1  # Ensure run twice each window
-    }
-    $week_days = if 'week_days' in $window {
-      $window['week_days']
-    } else {
-      ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    }
-    $duration = Timespan($hours * 60 * 60)
-    $today_start = Timestamp.new("${today_date} ${$window['start_time']}", '%F %k:%M', $tz)
-    $today_end = $today_start + $duration
-    $today_week_day = $today_start.strftime('%A', $tz)
-
-    debug('Today', $today_week_day, $today_start, $today_end)
-
-    if $today_start <= $now and $today_end > $now and $today_week_day in $week_days {
-      true
-    } else {
-      $today_begin = Timestamp.new($today_date, '%F', $tz)
-      $yesterday_date = ($today_begin - 1).strftime('%F', $tz)
-      $yesterday_start = Timestamp.new("${yesterday_date} ${$window['start_time']}", '%F %k:%M', $tz)
-      $yesterday_end = $yesterday_start + $duration
-      $yesterday_week_day = $yesterday_start.strftime('%A', $tz)
-
-      debug('Yesterday', $yesterday_week_day, $yesterday_start, $yesterday_end)
-
-      $yesterday_start <= $now and $yesterday_end > $now and $yesterday_week_day in $week_days
-    }
-  }
+  $downtime::is_downtime
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,65 @@
 class downtime (
-  Boolean $restricted = false,
-  Struct[{
-    start_time          => Pattern[/[012]?\d:\d\d/],
-    Optional[week_days] => Array[Enum['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']],
-    Optional[hours]     => Variant[Float, Integer],
-  }] $window          = { start_time => '3:00' },
+  Variant[
+    Enum[always, never],
+    Struct[{
+      start_time          => Pattern[/[012]?\d:\d\d/],
+      Optional[week_days] => Array[Enum[monday, tuesday, wednesday, thursday, friday, saturday, sunday]],
+      Optional[hours]     => Variant[Float, Integer],
+    }]
+  ] $when = always,
 ) {
 
   include timezone
 
-  if $restricted {
-    if $timezone::timezone == 'Etc/UTC' {
-      fail('You need to specify timezone::timezone to restrict downtime')
-    } elsif $facts['timezone'] == 'UTC' {
-      warning(
-        'Reported timezone is UTC, assuming timezone being applied and not in facts yet but will be next run, will report as outside window'
-      )
-    }
-    if !$timezone::autoupgrade {
-      notice('Suggest set timezone::autoupgrade to keep timezone data up to date')
+  $is_downtime = case $when {
+    always: { true }
+    never: { false }
+    default: {
+      if !$timezone::autoupgrade {
+        notice('Suggest set timezone::autoupgrade to keep timezone data up to date')
+      }
+      $tz = $facts['timezone']
+      if $timezone::timezone == 'Etc/UTC' {
+        fail('You need to specify timezone::timezone to restrict downtime')
+      } elsif $tz == 'UTC' {
+        warning(
+          'Reported timezone is UTC, assuming timezone being applied this run and not in facts yet, treating as not downtime'
+        )
+        false
+      } else {
+        $now = Timestamp(time())
+        debug('Now', $now)
+
+        $today_date = $now.strftime('%F', $tz)
+        $hours = if 'hours' in $when {
+          $when['hours']
+        } else {
+          1.1  # Ensure run twice each window
+        }
+        $week_days = if 'week_days' in $when {
+          $when['week_days']
+        } else {
+          [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
+        }
+        $duration = Timespan($hours * 60 * 60)
+        $today_start = Timestamp.new("${today_date} ${$when['start_time']}", '%F %k:%M', $tz)
+        $today_end = $today_start + $duration
+        $today_week_day = $today_start.strftime('%A', $tz).downcase
+        debug('Today', $today_week_day, $today_start, $today_end)
+
+        if $today_start <= $now and $today_end > $now and $today_week_day in $week_days {
+          true
+        } else {
+          $today_begin = Timestamp.new($today_date, '%F', $tz)
+          $yesterday_date = ($today_begin - 1).strftime('%F', $tz)
+          $yesterday_start = Timestamp.new("${yesterday_date} ${$when['start_time']}", '%F %k:%M', $tz)
+          $yesterday_end = $yesterday_start + $duration
+          $yesterday_week_day = $yesterday_start.strftime('%A', $tz).downcase
+          debug('Yesterday', $yesterday_week_day, $yesterday_start, $yesterday_end)
+
+          $yesterday_start <= $now and $yesterday_end > $now and $yesterday_week_day in $week_days
+        }
+      }
     }
   }
 
